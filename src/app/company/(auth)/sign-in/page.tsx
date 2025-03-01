837d17986dd5d754746634d24dda0292de4065e5
farhanhrywn <70428231+farhanhrywn@users.noreply.github.com>
2025-03-01 16:53:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/company/(auth)/sign-in/page.tsx
@@ -0,0 +1,44 @@
+import Title from "antd/es/typography/Title";
+import Text from "antd/es/typography/Text";
+import Image from "next/image";
+import Link from "antd/es/typography/Link";
+import NextLink from "next/link";
+import { Button, Input } from "antd";
+import Password from "antd/es/input/Password";
+
+function SignInPage() {
+    return (
+        <div className="w-screen h-screen">
+            <div className="flex justify-start">
+                <div className="h-screen w-[400px]" style={{
+                    backgroundImage: 'url(/images/sign-in-thumbnail.png)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center center',
+                    backgroundSize: 'cover',
+                }} />
+                <div className="bg-white flex-1 flex items-center px-20">
+                    <div className="w-[396px] flex flex-col gap-12">
+                        <Image src={'/images/mandiri-company-logo.png'} width={278} height={64} alt="Logo" />
+                        <div className="flex flex-col gap-6">
+                            <div>
+                                <Title level={2} style={{ margin: 0 }}>Masuk</Title>
+                                <Text type="secondary">Rekrut kandidat yang tepat untuk bisnis anda.</Text>
+                            </div>
+                            <Input placeholder="Email" />
+                            <Password placeholder="Password" />
+                            <NextLink href={'/company/dashboard'}>
+                                <Button type="primary">Masuk</Button>
+                            </NextLink>
+                        </div>
+                        <div className="flex gap-1">
+                            <Text>Belum punya akun?</Text>
+                                <Link href={'/company/sign-up'}>Daftar</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignInPage;
---
+++ src/app/company/(auth)/sign-up/_components/step-1.tsx
@@ -0,0 +1,70 @@
+'use client'
+import Title from "antd/es/typography/Title";
+import Text from "antd/es/typography/Text";
+import Image from "next/image";
+import { Button, Card, Input, Select } from "antd";
+import { useState } from "react";
+
+function SignUpFormStep1({
+    nextStep
+}: {
+    nextStep: () => void;
+}) {
+    const [values, setValues] = useState({
+        name: '',
+        lastName: '',
+        businessName: '',
+        country: undefined,
+        phone: ''
+    })
+    const disable = Object.values(values).some(value => !value)
+    return (
+        <div className="w-screen h-screen">
+            <div className="bg-white border-b border-[#E0E0E0] py-2.5 px-2.5">
+                <div className="container mx-auto">
+                    <Image src={'/images/logo-mandiri.svg'} alt="Logo" height={48} width={120} />
+                </div>
+            </div>
+            <div className="flex justify-center py-12">
+                <div className="w-[588px]">
+                    <Card>
+                        <div className="flex flex-col gap-6">
+                            <Title level={2} style={{ margin: 0 }}>Buat Akun Perusahaan</Title>
+                            <Text>Masukan beberapa detail tentang bisnis Anda untuk memverifikasi Akun. Kami tidak akan membagikan detail informasi Anda dengan siapa pun.</Text>
+                            <div className="flex gap-3">
+                                <Input placeholder="Nama Depan" value={values.name} onChange={(e) => setValues((values) => ({ ...values, name: e.target.value }))} />
+                                <Input placeholder="Nama Belakang" value={values.lastName} onChange={(e) => setValues((values) => ({ ...values, lastName: e.target.value }))} />
+                            </div>
+                            <div className="flex gap-3">
+                                <Input placeholder="Nama Bisnis" value={values.businessName} onChange={(e) => setValues((values) => ({ ...values, businessName: e.target.value }))} />
+                            </div>
+                            <div className="flex gap-3">
+                                <Select
+                                    className="w-full"
+                                    placeholder="Negara"
+                                    options={[
+                                        { value: 'Indonesia', label: 'Indonesia' },
+                                        { value: 'Malaysia', label: 'Malaysia' },
+                                        { value: 'Singapura', label: 'Singapura' },
+                                        { value: 'Thailand', label: 'Thailand' },
+                                    ]}
+                                    value={values.country} onChange={(e) => setValues((values) => ({ ...values, country: e }))}
+                                />
+                            </div>
+                            <div className="flex gap-3">
+                                <Input placeholder="Nomor Telepon" value={values.phone} onChange={(e) => setValues((values) => ({ ...values, phone: e.target.value }))} />
+                            </div>
+                            <div>
+                                <Button type="primary" onClick={nextStep} disabled={disable} >
+                                    Buat Akun
+                                </Button>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignUpFormStep1;
